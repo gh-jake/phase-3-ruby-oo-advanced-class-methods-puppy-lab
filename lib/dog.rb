46909ceea92885814cdcb91548bfb0ebec4df663
Jake Houston <jakechouston@gmail.com>
2021-05-21 14:11:03 -0400

    commit tree 079ee3af00a776d1d24b67132bac87ff57bbe35c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+class Dog
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    attr_accessor :name
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |puppy| 
+            puts puppy.name
+        end
+    end
+
+    def self.clear_all
+        @@all = @@all.clear
+    end
+end
